235a3cd7f4441f6f29c2c8e2ee9533c5a24af28d
CristianBerta <zcristianb.05@gmail.com>
2024-05-10 19:33:03 -0300

    Clase 02

--- js/script.js
+++ js/script.js
@@ -1,66 +1,117 @@
-console.log("Hola cristian");
+let unNumero;
 
-console.log("-------------------------");
+unNumero = 10;
 
-// Declaracion de variables usando ESS
+unNumero = 12;
 
-var palabra = "hola cristian";
+/* Muestra un mensaje de alerta que indique si el usuario es 
+menor de edad o mayor de edad, dependiendo de su 
+edad ingresada */
 
-console.log(palabra); //imprimo palabra
+// let edadIngresada = prompt("Ingrese su Edad: ");
 
-palabra = "como estas";
+// if (edadIngresada >= 18) //Condicion correcta para Mayor de edad
+//     {
+//         alert("Su edad es de: " + edadIngresada + " Años, entonces es Mayor de Edad");
+//     }
+// else if (edadIngresada > 0 && edadIngresada < 18) //La edad tiene que ser mayor a 0 Y menor a 18
+//     {
+//         alert("Su edad es de: " + edadIngresada + " Años, entonces es Menor de Edad");
+//     } 
+// else if(edadIngresada <= 0) // Para edad Menor o Igual a 0
+//     {
+//         alert("La Edad ingresada debe ser Mayor a 0 !!");
+//     } 
+// else
+//     {
+//         alert("La Edad ingresada es Incorrecta!!!");
+//     }
 
-console.log(palabra); //reasigno palabra y la imprimo
+/*------------------------------------------------------------------------------------*/
 
-var palabra = "Bien?";
+let unaVariable = -1;
 
-console.log(palabra); //al escribir var creo una nueva variable sin importar que tenga el mismo nombre
+unaVariable = 2;
 
-// Declaracion de variables usando ES6
+//Valores Booleanos (Son true o false)
 
-let palabra00 = "Nueva palabra con ES6";
+console.log(unaVariable > 0) // La condicion es true o false
 
-console.log(palabra00);
+/*
+    if (condicion) Si la condicion es true "Verdadera"
+        { 
+            <<<<< Ejecuto esto >>>>>
+        }
+        Sino ejecuto esto: (Que puede ser otra condicion o un else)
+        else 
+        {
+            <<<<< Ejecuto esto >>>>>
+        }
 
-palabra00 = "Reasignacion";
+*/
 
-console.log(palabra00);
+/*-----------------------------------------------------------------------------*/
 
-// let palabra00 = "Hola" no funciona como var en ESS, no se puede volver a declarar el mismo nombre
+//Tablas de Verdad
 
-let palabra01 = "Nueva palabra01";
+// const V = true;
+// const F = false;
 
-console.log(palabra01);
+// console.log("Tablas de Verdad del Operador && (Y)");
+// console.log(V && V);
+// console.log(V && F);
+// console.log(F && V);
+// console.log(F && F);
 
-// Declaracion de constante
+// console.log("Tablas de Verdad del Operador || (O)");
+// console.log(V || V);
+// console.log(V || F);
+// console.log(F || V);
+// console.log(F || F);
 
-const CONSTANTE00 = "constante01"; // Las constantes siempre en mayuscula
-console.log(CONSTANTE00);
+// console.log("Tablas de Verdad de la Negacion !");
+// console.log(!V + " La negacion de la Verdad");
+// console.log(!F + " la negacion de la Falsedad");
 
-// CONSTANTE00 = "constante00" como es constante no se puede reasignar
+/*---------------------------------------------------------------------*/
 
-//Mala practica
-let nombre1 = "cristian";
-let nombre2 = "alejandro";
+let ingreseSuNombre = prompt("Ingrese su Nombre:"); // Variable Global
+let ingreseSuApellido = prompt("Ingrese su Apellido:");
+const VACIO = ""; // Esta constante esta Vacia
 
-//Buena practica usando camelCase
-let primerNombre = "cristian"; //siempre se empieza con minuscula y las demas palabras con mayuscula
-let segundoNombre = "alejandro";
 
-let nombreCompletoDeLaPersona = "Cristian Alejandro Berta";
+// if (ingreseSuNombre != VACIO && ingreseSuApellido != VACIO)
+//     {
+//         let ingreseSuEdad = prompt("Ingrese su Edad:");
+//         if(ingreseSuEdad != VACIO)
+//             {
+//                 alert("Su nombre es: " + ingreseSuNombre + " " + ingreseSuApellido + " y tiene " + ingreseSuEdad + " Años de Edad");
+//             }
+//         else
+//             {
+//                 alert("Su nombre es: " + ingreseSuNombre + " " + ingreseSuApellido);
+//             }
+//     }
+// else{
+//     alert("Falta ingresar Informacion");
+//     }
 
-const MAYUSCULA = "En mayuscula";
+// console.log(ingreseSuNombre != VACIO && ingreseSuApellido != VACIO) //Si no ingreso alguno de los datos requeridos me sale false
 
-let anio = 2024;
-let flotante = 25.5;
+if (ingreseSuNombre != VACIO || ingreseSuApellido != VACIO)
+    {
+        let ingreseSuEdad = prompt("Ingrese su Edad:");
+        if(ingreseSuEdad != VACIO)
+            {
+                alert("Su nombre es: " + ingreseSuNombre + " " + ingreseSuApellido + " y tiene " + ingreseSuEdad + " Años de Edad");
+            }
+        else{
+                alert("Su nombre es: " + ingreseSuNombre + " " + ingreseSuApellido);
+            }
+    }
+else{
+        alert("Falta ingresar Toda la Informacion");
+    }
 
-// Operaciones basicas
-let a = 1;
-let b = 2;
-const RESULTADO = a + b;
-console.log(RESULTADO); //lo mismo para todas las operaciones
-
-const NOMBRE_COMPLETO = nombre1 + " " + nombre2; //concatenacion de palabras
-console.log(NOMBRE_COMPLETO);
-
-console.log("--------------------------");
+console.log(ingreseSuNombre != VACIO || ingreseSuApellido != VACIO) //Si  ingreso almenos uno de los datos requeridos me sale true
+console.log(ingreseSuEdad); //Variable Local
--- js/script_prompt.js
+++
@@ -1,7 +0,0 @@
-let primerNombreDelUsuario = prompt("Ingrese su primer nombre");
-console.log(primerNombreDelUsuario);
-
-const EDAD = prompt("Ingrese su edad");
-alert("Usted tiene " + " " + EDAD + " años"); //alert imprime con una ventana, no por consola
-
-console.log("Su nombre es " + primerNombreDelUsuario + " y tiene " + EDAD + " años"); //lo mismo con alert
