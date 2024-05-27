d3127833fbfb022a9420417d12b32b1e4adbc74b
CristianBerta <zcristianb.05@gmail.com>
2024-05-27 20:37:20 -0300

    Clase07

--- js/script.js
+++ js/script.js
@@ -1,128 +1,151 @@
-// // Arrays
-// const arrayVacio = [];
+// Funciones de orden superior
+//Analicemos este codigo:
+/**
+ * Se declara una variable que, a través de un iterador, 
+ * va acumulando la suma del contador:
+ */
 
-// // Tipo de datos dentro del Array
+// function sumarRango(inicio, fin) {
+//     let total = 0;
+//     for (let i = inicio; i <= fin; i++) {
+//         total += i;
+//     }
+//     return total;
+// }
+// console.log(sumarRango(1, 100));
+// console.log(sumarRango(1, 60));
+// console.log(sumarRango(0, 160));
 
-// const arrayNumerico = [1, 2, 3, 4, 5, 5.5]; // Tipo Numerico
 
-// const arrayConLetras = ['a', 'b', 'c']; // Tipo caracter
-
-// // const arrayConString = ["Hola", "mundo", "Como", "va?"]; // Tipo String
-
-// const arrayBoolean = [true, false, true]; // Tipo Booleano
-
-// // const arrayMixto = ['a', 23, "Hola", true]; // Tipo mixto
-
-// const arrayObjetos = [
-//     {nombre: "Producto1", cantidad: 5},
-//     {nombre: "Producto2", cantidad: 8}
-// ]; // Tipo Objetos
-
-const arrayMixtoConObjetos = ['a', 23, "Hola", true, 
-{nombre: "Producto3", cantidad: 8}]; // Tipo mixto Con Objetos
-
-// console.log(arrayNumerico[0]);
-// console.log(arrayObjetos[1]);
-
-// for (let index = 0; index < 6; index++) {
-//     //alert(arrayNumerico[index]);
+// function mayorQue(numero) {
+//     return (numeroAComparar) => numeroAComparar > numero; // Retorna una Funcion Anonima
 // }
 
-// for (let index = 0; index < 5; index++) {
-//     //alert(arrayMixtoConObjetos[index]);
-// }
-//console.log(arrayNumerico.length);
-//console.log(arrayMixtoConObjetos.length);
+// // const algo = 0;
+// const mayorQueDiez = mayorQue(10); // let mayorQueDiez = (numeroAComparar) => numeroAComparar > 10;
+// // const mayorQueDiez = (numeroAComparar) => numeroAComparar > numero; 
+// console.log( mayorQueDiez(12) ); // numeroAComparar = 12 // true
+// console.log( mayorQueDiez(8) ); // numeroAComparar = 8 // false
 
-// for (let index = 0; index < arrayMixtoConObjetos.length; index++) {
-//     console.log(arrayMixtoConObjetos[index]);
-// }
-
-// Agregar elementos a un Array
-// arrayMixto.push("Al final"); // Agrega un elemento al Array al final
-// console.log(arrayMixto);
-// arrayMixto.unshift("Al principio"); // Agrega un elemento al Array al principio
-// console.log(arrayMixto);
-// arrayMixto.pop();
-// console.log(arrayMixto);// Elimina el elemento al Array del final
-// arrayMixto.shift();
-// console.log(arrayMixto);// Elimina el elemento al Array del principio
-// arrayMixto.splice(1, );
-//console.log(arrayMixto);
-// console.log(arrayConString);
-// arrayConString.splice(2, 2);
-// console.log(arrayConString);
-// arrayConString.splice(1, 2);
-// console.log(arrayConString);
-
-// Uso de join
-// const arrayConString = ["Hola", "mundo", "Como", "va?"];
-// console.log( arrayConString.join(", "));
-// console.log( arrayConString.join(" "));
-// console.log( arrayConString.join("* "));
-// console.log( arrayConString.join(" Una Frase "));
-
-// Uso de concat
-// const arrayConString = ["Hola", "mundo", "Como", "va?"];
-// const arrayMixto = ['a', 23, "Hola", true];
-// console.log(arrayConString.concat(arrayMixto));
-
-// Uso de slice
-// const nuevoArrayConString = arrayConString.slice(0, 2);
-// console.log(nuevoArrayConString);
-// console.log(nuevoArrayConString.indexOf("Hola"));
-// console.log(nuevoArrayConString.indexOf("mundo"));
-// console.log(nuevoArrayConString.indexOf("Mundo"));
-// console.log(nuevoArrayConString.indexOf("Como"));
-// // Uso de include
-// console.log(nuevoArrayConString.includes("Hola"));
-// console.log(nuevoArrayConString.includes("mundo"));
-// console.log(nuevoArrayConString.includes("Mundo"));
-// console.log(nuevoArrayConString.includes("Como"));
-
-// Uso de reverse
-// console.log(arrayConString);
-// console.log(arrayConString.reverse());
-
-// const arrayObjetos = [
-//     {nombre: "Producto1", cantidad: 5},
-//     {nombre: "Producto2", cantidad: 8}
-// ];
-
-// arrayObjetos.push({nombre: "Producto3", cantidad: 58});
-
-// console.log(arrayObjetos);
-// const productos = [
-//     { id: 1, nombre: "Arroz" },
-//     { id: 2, nombre: "Fideo" },
-//     { id: 3, nombre: "Pan" }
-// ];
-
-// for (const index of productos) {
-//     console.log("El ID es: " + index.id);
-//     console.log("El Nombre del Producto es: " + index.nombre);
-//     //console.log(index);
+// function asignarOperacion(operacion){
+//     if(operacion == "sumar") {
+//         return (a, b) => a + b;
+//     } else if (operacion == "restar") {
+//         return (a, b) => a - b;
+//     }
 // }
 
-class Producto {
-    constructor(id, nombre, precio) {
-        this.id = id;
-        this.nombre = nombre.toUpperCase();
-        this.precio = parseFloat(precio);
+// const funcionSumar = asignarOperacion("sumar"); // const funcionSumar = (a, b) => a + b;
+// const funcionRestar = asignarOperacion("restar"); // const funcionRestar = (a, b) => a - b;
+
+// console.log(funcionSumar(4, 5)); // 9
+// console.log(funcionRestar(8, 3)); // 5
+
+// Recibir una Funcion por parametro
+
+// function porCadaElemento(myArray, myFuncion){
+//     for ( const elemento of myArray ) { // Por cada elemento de myArray, ejecuta la funcion
+//         myFuncion(elemento); // Esto se le aplique a cada elemento de myArray
+//     }
+// }
+
+// const numeros = [1, 2, 3, 4, 5, 6];
+// porCadaElemento(numeros, console.log); // Cuando paso una funcion como parametro no lleva ()
+// // Los parentesis los lleva cuando la defino, o cuando la invoco o la llamo.
+
+// let total = 0;
+// function acumuladora(numero) {
+//     total += numero;
+// }
+// porCadaElemento(numeros, acumuladora);
+// console.log("Total Acumulado: " + total);
+
+// const numerosDuplicados = [];
+
+// porCadaElemento(numeros, (elemento) => {
+//     numerosDuplicados.push(elemento * 2);
+// })
+
+// console.log(numerosDuplicados.join(", "));
+
+
+// For Each
+// numerosDuplicados.forEach( (numero) => {
+//     console.log(numero);
+// })
+
+// let nuevoArray = [];
+
+// numerosDuplicados.forEach( (numeros) => {
+//     nuevoArray.push(numeros - 1);
+// })
+
+// console.log(nuevoArray);
+
+// Find
+const cursos = [
+    {nombre: "JavaScript", cantAlumnos: 120},
+    {nombre: "JavaScript", cantAlumnos: 130},
+    {nombre: "React", cantAlumnos: 90},
+    {nombre: "PHP", cantAlumnos: 60}
+]
+console.log(cursos);
+
+const resultadoFind = cursos.find( (elemento) => elemento.nombre === "JavaScript");
+console.log(resultadoFind);
+// Filter
+const resultadoFilter = cursos.filter( (elemento) => elemento.nombre.includes("JavaScript"));
+console.log(resultadoFilter);
+
+// Some
+const resultadoSome = cursos.some( (elemento) => elemento.nombre === "JavaScript");
+const resultadoSomeFalse = cursos.some( (elemento) => elemento.nombre === "Javascript");
+console.log(resultadoSome);
+console.log(resultadoSomeFalse);
+
+// Map
+const resultadoMap = cursos.map((elemento) => elemento.nombre );
+console.log(resultadoMap);
+
+const actualizarCantidadDeAlumnos = cursos.map( (elemento) => {
+    return {
+        nombreNuevo: elemento.nombre,
+        cantidadNueva: elemento.cantAlumnos + 20
     }
+});
 
-    sumarIva() {
-        this.precio = this.precio * 1.21;
+console.log(actualizarCantidadDeAlumnos);
+
+// Reduce
+const numeros = [1, 2, 3, 4, 5, 6];
+
+const totalEnNumeros = numeros.reduce((acumulador, elemento) => acumulador + elemento, 10 );
+console.log(totalEnNumeros);
+
+// Sort
+const numerosDesordenados = [102, 24, 43, 4, 15, 96];
+console.log(numerosDesordenados);
+numerosDesordenados.sort((a, b) => a - b ); // Forma Ascendente
+console.log(numerosDesordenados);
+numerosDesordenados.sort((a, b) => b - a ); // Forma Descendente
+console.log(numerosDesordenados);
+
+// Sort con Objetos
+const cursosConSort = [
+    {nombre: "JavaScript", cantAlumnos: 120},
+    {nombre: "JavaScript", cantAlumnos: 130},
+    {nombre: "React", cantAlumnos: 90},
+    {nombre: "PHP", cantAlumnos: 60}
+]
+
+cursosConSort.sort((a, b) => {
+    if(a.cantAlumnos > b.cantAlumnos) {
+        return 1;
+    } 
+    if( a.cantAlumnos < b.cantAlumnos ) {
+        return -1;
     }
-}
+    return 0;
+})
 
-const carrito = [];
-
-carrito.push(new Producto(1, "Arroz", 1000.5));
-carrito.push(new Producto(2, "Azucar", 1200.5));
-carrito.push(new Producto(3, "Leche", 850));
-console.log(carrito);
-
-for (const porCadaProducto of carrito ) {
-    porCadaProducto.sumarIva();
-}
+console.log(cursosConSort);
