83e5c358d4bb2ae4c48f786aa32dd52d7ffaf2a3
CristianBerta <zcristianb.05@gmail.com>
2024-06-12 18:29:36 -0300

    clase12

--- js/navegacion.js
+++ js/navegacion.js
@@ -1,18 +1,3 @@
-// Delaración de Variables
-const cuerpo = document.body;
-const cabecera = document.getElementById('header');
-const navegacion = document.createElement('div'); 
-const nav = document.createElement('nav');
-const ul = document.createElement('ul');
-const links = ["Index", "Products", "Contact"];
-const liImagen = document.createElement('li');
-const img = document.createElement('img');
-const ORIGEN = document.createElement('a');
-const footer = document.getElementsByTagName('footer');
-const parrafoFooter = document.createElement('p');
-const TIME = new Date().getFullYear(); // 2024
-
-
 cabecera.appendChild(navegacion);
 navegacion.appendChild(nav);
 nav.appendChild(ul);
@@ -28,10 +13,11 @@ liImagen.appendChild(ORIGEN);
 ul.appendChild(liImagen);
 
 for (const link of links) {
-    const li = document.createElement('li');
+    const li = document.createElement('li');    
     li.innerHTML = `<a href="${link.toLowerCase()}.html" >${link}</a>`;
     ul.appendChild(li);
 }
+
 
 cabecera.style.backgroundColor = '#c2c2c2';
 
@@ -41,7 +27,7 @@ parrafoFooter.innerHTML = 'CoderHouse - ' + TIME + ' | Comisión #57705';
 
 document.addEventListener('DOMContentLoaded', function() {
     const buttonToggle = document.getElementById('toggle-button');
-
+    buttonToggle.className = 'button_grey';
     buttonToggle.addEventListener('click' , function() {
         cuerpo.classList.toggle('dark-mode');
         updateButtonText();
