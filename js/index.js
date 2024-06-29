2f3b72a421327ea6636952ea3507fa23ac2769c4
CristianBerta <zcristianb.05@gmail.com>
2024-06-29 14:06:04 -0300

    GaleriaConSwiper

--- js/index.js
+++ js/index.js
@@ -1,48 +1,62 @@
 // js/index.js
-document.addEventListener('DOMContentLoaded', () => {
-    const images = [
-        'img/img1.jpg',
-        'img/img2.jpg',
-        'img/img3.jpg',
-        'img/img4.jpg',
-        'img/img5.jpg',
-        'img/img6.jpg',
-        'img/img7.jpg'
-        // Agrega más rutas de imágenes según sea necesario
-    ];
+// document.addEventListener('DOMContentLoaded', () => {
+//     const images = [
+//         'img/img1.jpg',
+//         'img/img2.jpg',
+//         'img/img3.jpg',
+//         'img/img4.jpg',
+//         'img/img5.jpg',
+//         'img/img6.jpg',
+//         'img/img7.jpg'
+//         // Agrega más rutas de imágenes según sea necesario
+//     ];
 
-    const slideContainer = document.querySelector('.slide');
-    images.forEach((image, index) => {
-        const li = document.createElement('li');
-        const img = document.createElement('img');
-        img.src = image;
-        if (index === 0) li.classList.add('active');
-        li.appendChild(img);
-        slideContainer.appendChild(li);
-    });
+//     const slideContainer = document.querySelector('.slide');
+//     images.forEach((image, index) => {
+//         const li = document.createElement('li');
+//         const img = document.createElement('img');
+//         img.src = image;
+//         if (index === 0) li.classList.add('active');
+//         li.appendChild(img);
+//         slideContainer.appendChild(li);
+//     });
 
-    let currentIndex = 0;
-    const slides = document.querySelectorAll('.slide li');
+//     let currentIndex = 0;
+//     const slides = document.querySelectorAll('.slide li');
 
-    function showImage(index) {
-        slides.forEach((slide, i) => {
-            slide.classList.toggle('active', i === index);
-        });
-    }
+//     function showImage(index) {
+//         slides.forEach((slide, i) => {
+//             slide.classList.toggle('active', i === index);
+//         });
+//     }
 
-    function changeImage(direction) {
-        currentIndex += direction;
-        if (currentIndex < 0) {
-            currentIndex = slides.length - 1;
-        } else if (currentIndex >= slides.length) {
-            currentIndex = 0;
-        }
-        showImage(currentIndex);
-    }
+//     function changeImage(direction) {
+//         currentIndex += direction;
+//         if (currentIndex < 0) {
+//             currentIndex = slides.length - 1;
+//         } else if (currentIndex >= slides.length) {
+//             currentIndex = 0;
+//         }
+//         showImage(currentIndex);
+//     }
 
-    document.querySelector('.icon-prev').addEventListener('click', () => changeImage(-1));
-    document.querySelector('.icon-next').addEventListener('click', () => changeImage(1));
+//     document.querySelector('.icon-prev').addEventListener('click', () => changeImage(-1));
+//     document.querySelector('.icon-next').addEventListener('click', () => changeImage(1));
     
-    // Mostrar la primera imagen
-    showImage(currentIndex);
+//     // Mostrar la primera imagen
+//     showImage(currentIndex);
+// });
+
+// --------swiper---------
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    effect: "flip",
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
